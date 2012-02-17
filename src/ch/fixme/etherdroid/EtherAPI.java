a253d18e2d63865ed3c95c43a28ee5e2a447b2a3
Rorist <aubort.jeanbaptiste@gmail.com>
2012-02-17 23:01:39 +0100

    EtherApi structure

---
+++ src/ch/fixme/etherdroid/EtherAPI.java
@@ -0,0 +1,15 @@
+package ch.fixme.etherdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EtherAPI
+{
+
+    void EtherAPI() {
+    }
+
+    void getText(String padId, String revision) {
+
+    }
+}
